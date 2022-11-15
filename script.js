8f444d41daeada727c6abb56655762029ce8caf9
EmmaKlein99 <emma009g@easv365.dk>
2022-11-15 08:45:23 +0100

    gdhijolskq

---
+++ script.js
@@ -0,0 +1,10 @@
+const bright = document.querySelector(".neonText");
+
+function showBright() {
+  const bright = document.querySelector(".neonText");
+  if (bright.style.display === "none") {
+    bright.style.display = "block";
+  } else {
+    bright.style.display = "none";
+  }
+}
